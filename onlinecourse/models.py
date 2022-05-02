5187b2bc6719166a2bd69b871fdbea90b8b095de
Muralidhara Bhat <ksmuralidhara0@gmail.com>
2022-05-02 12:48:07 +0000

    lab2

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,11 +101,23 @@ class Enrollment(models.Model):
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
+
+class Question(models.Model):
     # Foreign key to lesson
     # question text
     # question grade/mark
-
+    question_text = models.CharField(max_length=200)
+    grade = models.FloatField(default=0.0)
+    lesson = models.ForeignKey(Lesson, null=False, on_delete=models.CASCADE)
+    courses = models.ManyToManyField(Course)
+    # <HINT> A sample model method to calculate if learner get the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
     # <HINT> A sample model method to calculate if learner get the score of the question
     #def is_get_score(self, selected_ids):
     #    all_answers = self.choice_set.filter(is_correct=True).count()
@@ -123,7 +135,11 @@ class Enrollment(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 # class Choice(models.Model):
-
+class Choice(models.Model):
+     choice_text = models.CharField(null=False, max_length=50)
+    #  questions = models.ManyToManyField(Question, null=False)
+     is_correct = models.BooleanField(default=True)
+     question_id = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
